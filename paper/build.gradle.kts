5686b0a543a557ee8c4c34e49a90798b5fc59bf0
4drian3d <adrianed2004@gmail.com>
2022-03-13 10:58:58 -0500

    Test Paperweight

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
     id("net.minecrell.plugin-yml.bukkit") version "0.5.1"
-    //id("io.papermc.paperweight.userdev") version "1.3.5"
+    id("io.papermc.paperweight.userdev") version "1.3.5"
     id("xyz.jpenilla.run-paper") version "1.0.6"
 }
 
 dependencies {
     compileOnly(project(":miniplaceholders-common"))
     compileOnly(project(":miniplaceholders-api"))
-    //paperDevBundle("1.18.2-R0.1-SNAPSHOT")
+    paperDevBundle("1.18.2-R0.1-SNAPSHOT")
     
-    compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
-    compileOnly("io.papermc.paper:paper-mojangapi:1.18.2-R0.1-SNAPSHOT")
+    //compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
+    //compileOnly("io.papermc.paper:paper-mojangapi:1.18.2-R0.1-SNAPSHOT")
 }
 
 val pluginVersion = version
