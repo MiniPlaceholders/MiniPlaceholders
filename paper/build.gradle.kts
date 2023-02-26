70e8d8925dc508b48efc8220e683c7447e6d427e
Adrian <adriangonzalesval@gmail.com>
2023-02-26 00:10:44 -0500

    fix: update plugin-yml plugin

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("net.minecrell.plugin-yml.bukkit") version "0.5.2"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.3"
     id("io.papermc.paperweight.userdev") version "1.3.8"
     id("xyz.jpenilla.run-paper") version "1.0.6"
 }
