bc6d51e07c4d4fc2e8ba64d759cf234cc45e2d76
Adrian <adriangonzalesval@gmail.com>
2023-05-13 10:43:56 -0500

    api: Improved conditional API methods

--- api/src/main/java/io/github/miniplaceholders/api/utils/Conditions.java
+++ api/src/main/java/io/github/miniplaceholders/api/utils/Conditions.java
@@ -1,36 +1,73 @@
 package io.github.miniplaceholders.api.utils;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.util.function.Supplier;
 
 /** Checks required for the correct operation of the expansions*/
 public final class Conditions {
-    private Conditions(){}
+    private Conditions() {}
 
     /**
-     * Checks if a string is null or empty or consists of whitespace characters only
+     * Checks if a string is null or empty or consists of whitespace characters only.
+     *
      * @param string the string
      * @param name the name supplier
      * @return the string if it is not null or empty
      * @since 1.0.0
+     * @throws IllegalStateException if the provided string is empty
      */
-    public static String nonNullOrEmptyString(String string, Supplier<String> name){
+    public static String nonNullOrEmptyString(@Nullable String string, @NotNull Supplier<@NotNull String> name){
         if (string == null)
-            throw new NullPointerException("the " + name.get() + " cannot be null");
+            throw new NullPointerException(name.get() + " cannot be null");
         if (string.isBlank())
-            throw new IllegalStateException("the " + name.get() + "cannot be an empty string");
+            throw new IllegalStateException(name.get() + "cannot be an empty string");
         return string;
     }
 
     /**
-     * Check if a string is empty or consists of whitespace characters only
+     * Checks if a string is null or empty or consists of whitespace characters only.
+     *
+     * @param string the string
+     * @param name the name
+     * @return the string if it is not null or empty
+     * @since 2.2.0
+     * @throws IllegalStateException if the provided string is empty
+     */
+    public static String nonNullOrEmptyString(@Nullable String string, @NotNull String name){
+        if (string == null)
+            throw new NullPointerException(name + " cannot be null");
+        if (string.isBlank())
+            throw new IllegalStateException(name + "cannot be an empty string");
+        return string;
+    }
+
+    /**
+     * Check if a string is empty or consists of whitespace characters only.
+     *
      * @param string the string
      * @param reason the reason supplier
      * @return the string if it not empty
      * @since 1.0.0
+     * @throws IllegalStateException if the provided string is empty
      */
-    public static String nonEmptyString(String string, Supplier<String> reason){
-        if (string.isBlank())
-            throw new IllegalStateException(reason.get());
+    public static String nonEmptyString(@NotNull String string, @NotNull Supplier<@NotNull String> reason){
+        if (string.isBlank()) throw new IllegalStateException(reason.get());
+        return string;
+    }
+
+    /**
+     * Check if a string is empty or consists of whitespace characters only.
+     *
+     * @param string the string
+     * @param reason the reason
+     * @return the string if it not empty
+     * @since 2.2.0
+     * @throws IllegalStateException if the provided string is empty
+     */
+    public static String nonEmptyString(@NotNull String string, @NotNull String reason){
+        if (string.isBlank()) throw new IllegalStateException(reason);
         return string;
     }
     
