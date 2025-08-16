88fed91f6240dd21db551ec3411876d112a7e63a
Adrian Gonzales <adriangonzalesval@gmail.com>
2025-08-16 14:38:58 -0500

    fix: Expansions initialization on Paper

--- paper/src/main/java/io/github/miniplaceholders/paper/PaperBootstrap.java
+++ paper/src/main/java/io/github/miniplaceholders/paper/PaperBootstrap.java
@@ -1,6 +1,7 @@
 package io.github.miniplaceholders.paper;
 
 import io.github.miniplaceholders.common.command.PlaceholdersCommand;
+import io.github.miniplaceholders.connect.InternalPlatform;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.plugin.bootstrap.BootstrapContext;
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
@@ -18,32 +19,33 @@ import java.util.stream.Collectors;
 
 @SuppressWarnings({"unused", "UnstableApiUsage"})
 public final class PaperBootstrap implements PluginBootstrap {
-  @Override
-  public void bootstrap(@NotNull BootstrapContext bootstrapContext) {
-    PaperCommandManager<SourceWrapper> commandManager = PaperCommandManager
-            .builder(SenderMapper.create(SourceWrapper::new, SourceWrapper::stack))
-            .executionCoordinator(ExecutionCoordinator.simpleCoordinator())
-            .buildBootstrapped(bootstrapContext);
-
-    PlaceholdersCommand.<SourceWrapper>builder()
-            .playerSuggestions(() -> Bukkit.getServer().getOnlinePlayers()
-                    .stream()
-                    .map(Player::getName)
-                    .collect(Collectors.toCollection(ArrayList::new)))
-            .toAudience(st -> Bukkit.getServer().getPlayer(st))
-            .hasPermissionCheck((aud, p) -> aud.stack.getSender().hasPermission(p))
-            .manager(commandManager)
-            .command("miniplaceholders")
-            .build()
-            .register();
-
-  }
-
-  public record SourceWrapper(CommandSourceStack stack) implements ForwardingAudience.Single {
-
     @Override
-    public @NotNull Audience audience() {
-      return this.stack.getSender();
+    public void bootstrap(@NotNull BootstrapContext bootstrapContext) {
+        InternalPlatform.platform(InternalPlatform.PAPER);
+        PaperCommandManager<SourceWrapper> commandManager = PaperCommandManager
+                .builder(SenderMapper.create(SourceWrapper::new, SourceWrapper::stack))
+                .executionCoordinator(ExecutionCoordinator.simpleCoordinator())
+                .buildBootstrapped(bootstrapContext);
+
+        PlaceholdersCommand.<SourceWrapper>builder()
+                .playerSuggestions(() -> Bukkit.getServer().getOnlinePlayers()
+                        .stream()
+                        .map(Player::getName)
+                        .collect(Collectors.toCollection(ArrayList::new)))
+                .toAudience(st -> Bukkit.getServer().getPlayer(st))
+                .hasPermissionCheck((aud, p) -> aud.stack.getSender().hasPermission(p))
+                .manager(commandManager)
+                .command("miniplaceholders")
+                .build()
+                .register();
+
     }
-  }
+
+    public record SourceWrapper(CommandSourceStack stack) implements ForwardingAudience.Single {
+
+        @Override
+        public @NotNull Audience audience() {
+            return this.stack.getSender();
+        }
+    }
 }
