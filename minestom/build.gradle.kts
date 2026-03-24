68f8ce504cfff641fca6e5eadf06bcfcd5b1dd64
Leon-JavaScript <70959426+Leon-JavaScript@users.noreply.github.com>
2026-03-24 20:04:43 +0100

    feat(build): add Maven publishing configuration for Minestom

--- minestom/build.gradle.kts
+++ minestom/build.gradle.kts
@@ -1,7 +1,10 @@
+import java.time.LocalDate
+
 plugins {
     id("miniplaceholders.shadow")
     id("miniplaceholders.auto.module")
     id("miniplaceholders.runtask")
+    id("com.vanniktech.maven.publish")
 }
 
 dependencies {
@@ -21,4 +24,44 @@ dependencies {
 
 tasks.test {
     failOnNoDiscoveredTests = false
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates(project.group as String, "miniplaceholders-minestom", project.version as String)
+
+    pom {
+        name.set(project.name)
+        description.set(project.description)
+        inceptionYear.set(LocalDate.now().year.toString())
+        url.set("https://github.com/MiniPlaceholders/MiniPlaceholders")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("4drian3d")
+                name.set("Adrian Gonzales")
+                email.set("adriangonzalesval@gmail.com")
+            }
+        }
+        scm {
+            connection.set("scm:git:https://github.com/MiniPlaceholders/MiniPlaceholders.git")
+            developerConnection.set("scm:git:ssh://git@github.com/MiniPlaceholders/MiniPlaceholders.git")
+            url.set("https://github.com/MiniPlaceholders/MiniPlaceholders")
+        }
+        ciManagement {
+            name.set("GitHub Actions")
+            url.set("https://github.com/MiniPlaceholders/MiniPlaceholders/actions")
+        }
+        issueManagement {
+            name.set("GitHub")
+            url.set("https://github.com/MiniPlaceholders/MiniPlaceholders/issues")
+        }
+    }
 }
