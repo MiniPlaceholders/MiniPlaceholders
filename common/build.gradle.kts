3b616aeca8eed72a67af42e3c445b785a76bf8a5
Leon-JavaScript <70959426+Leon-JavaScript@users.noreply.github.com>
2026-03-31 17:27:05 +0200

    fix(build): add Maven publishing configuration for miniplaceholders-api and miniplaceholders-common
    This is necessary as the Minestom module needs to resolve these modules to properly build.

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,5 +1,8 @@
+import java.time.LocalDate
+
 plugins {
     id("miniplaceholders.auto.module")
+    id("com.vanniktech.maven.publish")
     alias(libs.plugins.idea.ext)
     alias(libs.plugins.blossom)
 }
@@ -23,6 +26,46 @@ sourceSets {
             javaSources {
                 property("version", project.version.toString())
             }
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates(project.group as String, "miniplaceholders-common", project.version as String)
+
+    pom {
+        name.set(project.name)
+        description.set(project.description)
+        inceptionYear.set(LocalDate.now().year.toString())
+        url.set("https://github.com/MiniPlaceholders/MiniPlaceholders")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("4drian3d")
+                name.set("Adrian Gonzales")
+                email.set("adriangonzalesval@gmail.com")
+            }
+        }
+        scm {
+            connection.set("scm:git:https://github.com/MiniPlaceholders/MiniPlaceholders.git")
+            developerConnection.set("scm:git:ssh://git@github.com/MiniPlaceholders/MiniPlaceholders.git")
+            url.set("https://github.com/MiniPlaceholders/MiniPlaceholders")
+        }
+        ciManagement {
+            name.set("GitHub Actions")
+            url.set("https://github.com/MiniPlaceholders/MiniPlaceholders/actions")
+        }
+        issueManagement {
+            name.set("GitHub")
+            url.set("https://github.com/MiniPlaceholders/MiniPlaceholders/issues")
         }
     }
 }
--- connect/build.gradle.kts
+++ connect/build.gradle.kts
@@ -1,3 +1,46 @@
+import java.time.LocalDate
+
 plugins {
     id("miniplaceholders.base")
+    id("com.vanniktech.maven.publish")
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates(project.group as String, "miniplaceholders-connect", project.version as String)
+
+    pom {
+        name.set(project.name)
+        description.set(project.description)
+        inceptionYear.set(LocalDate.now().year.toString())
+        url.set("https://github.com/MiniPlaceholders/MiniPlaceholders")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("4drian3d")
+                name.set("Adrian Gonzales")
+                email.set("adriangonzalesval@gmail.com")
+            }
+        }
+        scm {
+            connection.set("scm:git:https://github.com/MiniPlaceholders/MiniPlaceholders.git")
+            developerConnection.set("scm:git:ssh://git@github.com/MiniPlaceholders/MiniPlaceholders.git")
+            url.set("https://github.com/MiniPlaceholders/MiniPlaceholders")
+        }
+        ciManagement {
+            name.set("GitHub Actions")
+            url.set("https://github.com/MiniPlaceholders/MiniPlaceholders/actions")
+        }
+        issueManagement {
+            name.set("GitHub")
+            url.set("https://github.com/MiniPlaceholders/MiniPlaceholders/issues")
+        }
+    }
 }
